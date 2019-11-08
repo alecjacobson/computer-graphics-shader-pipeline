d15bc441fcec67d7b467b38cdac6860b28fb572b
Abhishek Madan <abhi.madan01@gmail.com>
2019-11-07 19:50:41 -0500

    Bump min OpenGL version

--- main.cpp
+++ main.cpp
@@ -70,8 +70,8 @@ int main(int argc, char * argv[])
   };
   glfwSetErrorCallback(error);
   glfwWindowHint(GLFW_SAMPLES, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   GLFWwindow* window = glfwCreateWindow(width, height, "shader-pipeline", NULL, NULL);
